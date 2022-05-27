508916685f0af674a08daf34327065bb110ce57a
Fredrik9087 <93259440+Fredrik9087@users.noreply.github.com>
2022-05-27 09:54:14 +0200

    MASSASDAJSNDJAKNDPSAIOÖBFÖ orkar inte jobba merrrrr

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const nav = document.getElementById("nav_links")
 const burger = document.getElementById("burger")
 const titles = document.getElementsByClassName('title')
+const arrow = document.getElementById("arrow")
+const discNav = document.getElementById("discNav")
 
 function ToggleMenu() {
     nav.classList.toggle("nav_active")
     burger.classList.toggle("crossed_line")
 }
 
-burger.addEventListener("click", ToggleMenu)
+//function ToggleDisc() {
+//discNav.classList.toggle("discNavActive")
+//arrow.classList.toggle("arrowCrossed")
+//}
 
+burger.addEventListener("click", ToggleMenu)
+    //arrow.addEventListener("click", ToggleDisc)
 
 for (let i = 0; i < titles.length; i++) {
     titles[i].style.border = `solid hsl(${Math.floor(Math.random() * 361)}, ${Math.floor((Math.random() * 41)+30)}%, ${Math.floor((Math.random() * 31)+35)}%) 1px`
 }
+
+function toBottom() {
+    window.scrollTo(0, document.getElementById("discussionBody").scrollHeight);
+}
+window.onload = toBottom;
