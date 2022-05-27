5af3ed57c984aa700c525195a13725f5e76c5bd9
Fredrik9087 <93259440+Fredrik9087@users.noreply.github.com>
2022-05-27 23:52:57 +0200

    Sista pushen. Äntligen.

--- js/script.js
+++ js/script.js
@@ -9,19 +9,25 @@ function ToggleMenu() {
     burger.classList.toggle("crossed_line")
 }
 
-//function ToggleDisc() {
-//discNav.classList.toggle("discNavActive")
-//arrow.classList.toggle("arrowCrossed")
-//}
-
 burger.addEventListener("click", ToggleMenu)
-    //arrow.addEventListener("click", ToggleDisc)
+
 
 for (let i = 0; i < titles.length; i++) {
     titles[i].style.border = `solid hsl(${Math.floor(Math.random() * 361)}, ${Math.floor((Math.random() * 41)+30)}%, ${Math.floor((Math.random() * 31)+35)}%) 1px`
 }
 
+
 function toBottom() {
     window.scrollTo(0, document.getElementById("discussionBody").scrollHeight);
 }
-window.onload = toBottom;
+window.onload = toBottom
+
+function ToggleDisc() {
+    discNav.classList.toggle("discNavActive")
+    document.getElementById("inputDisc").classList.toggle("inputDisc")
+    document.getElementById("searchHCont").classList.toggle("searchbarH")
+    document.getElementById("searchbarH").classList.toggle("searchbarH")
+    arrow.classList.toggle("crossedArrow")
+}
+
+arrow.addEventListener("click", ToggleDisc)
